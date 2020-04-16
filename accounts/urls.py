295ede9872fa587bb5b807664fd17fccba4f35d1
marek <marek559@vp.pl>
2020-04-16 21:29:32 +0200

    Implemented new login and register pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,12 @@ from . import views
 urlpatterns = [
     path('register/', views.register_page, name="register"),
     path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
+
     path('create_order/<str:pk>/', views.create_order, name="create_order"),
     path('update_order/<str:pk>/', views.update_order, name="update_order"),
     path('delete_order/<str:pk>/', views.delete_order, name="delete_order"),
