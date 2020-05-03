fcff42694ad85ba8b6e21b9657bb5ed91a00d419
marek <marek559@vp.pl>
2020-05-03 16:15:50 +0200

    initialize postgresql

--- accounts/tests/test_registration.py
+++ accounts/tests/test_registration.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_user_create():
-    User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    User.objects.create_user('john', 'lennon@thebeatles.com', 'password1')
     assert User.objects.count() == 1
